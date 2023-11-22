225af6959c2e42d5b09af15fcb2360fd1833e3a6
Clement Lin <Clement.Lin@amd.com>
2023-11-22 00:14:15 +0000

    Add maximum freq calculation

--- qgemm_run.py
+++ qgemm_run.py
@@ -1,6 +1,8 @@
 import subprocess
 import re
 import os
+import time
+import threading
 import openpyxl
 from openpyxl.styles import Font
 from openpyxl.utils import get_column_letter
@@ -46,7 +48,7 @@ def get_TFLOPS():
 def produce_report(command, tflops, peak_tflops):
     column_names = ['PRECISION_A', 'PRECISION_B', 'PRECISION_C', 'COMPUTE_PRECISION',
                     'OP_A', 'OP_B', 'M', 'N', 'K', 'LDA', 'LDB', 'LDC', 'BATCH_COUNT',
-                    'TIME_SPAN', 'TFLOPS', 'PEAK_TFLOPS', 'efficiency']
+                    'TIME_SPAN', 'EX', 'TFLOPS', 'PEAK_TFLOPS', 'efficiency']
 
     # Load an existing Excel workbook if it exists, otherwise create a new one
     try:
@@ -81,7 +83,9 @@ def produce_report(command, tflops, peak_tflops):
     sheet.cell(row=next_row, column=len(command) + 2 - 2, value=peak_tflops)
 
     # efficiency = tflops / peak_tflops * 100
-    efficiency = tflops / peak_tflops
+    efficiency = 0
+    if tflops != None and peak_tflops != 0:
+        efficiency = tflops / peak_tflops
     # sheet.cell(row=next_row, column=len(command) + 3 - 2, value=efficiency)
     # sheet[f"{openpyxl.utils.get_column_letter(len(command) + 3 - 2)}2"].number_format = '0.00%'
 
@@ -121,6 +125,28 @@ def get_mnk_list():
     #    print(f"m = {m}, n = {n}, k = {k}")
     return mnk_values
 
+def get_gpu_clock(max_clock, stop_event):
+    # clocks = []
+    try:
+        while not stop_event.is_set():
+            output = subprocess.check_output(['rocm-smi', '--showgpuclocks']).decode('utf-8')
+            lines = output.split('\n')
+            for line in lines:
+                if 'sclk clock level' in line:
+                    clock_level = int(line.split('(')[-1].split('Mhz')[0])
+                    print(f"GPU Clock: {clock_level} MHz")
+                    # clocks.append(clock_level)
+
+                    # max_clock[0] = max(clocks)  # Update max_clock if a new maximum is found
+                    # print("cocotion test = ", clock_level)
+                    # max_clock = clock_level
+
+                    max_clock[0] = max(max_clock[0], clock_level)
+                    # print("cocotion test @@ max_clock = ", max_clock)
+
+                    time.sleep(1)  # Wait for 1 second before capturing the next GPU clock
+    except subprocess.CalledProcessError as e:
+        print(f"Error running rocm-smi: {e}")
 
 try:
     # Attempt to remove the report.xlsx file
@@ -134,18 +160,20 @@ peak_tflops = 24.6
 type_dict = get_type_dict()
 mnk_values = get_mnk_list()
 
+# Create a list to store the maximum clock frequency
+max_clock = [0]
+
 # Define the command with parameters
 command = ['python3', 'qgemm.py', 'R_32F', 'R_32F', 'R_32F', 'R_32F', 'OP_N', 'OP_T',
-           '8640', '8640', '8640', '8640', '8640', '8640', '72', '300']
-
+           '8640', '8640', '8640', '8640', '8640', '8640', '72', '30']
 
 for type_key in type_dict:
     if type_key == "R_16F":
         command = ['python3', 'qgemm.py', 'R_32F', 'R_32F', 'R_32F', 'R_32F', 'OP_N', 'OP_T',
-            '8640', '8640', '8640', '8640', '8640', '8640', '72', '300', 'ex']
+            '8640', '8640', '8640', '8640', '8640', '8640', '72', '30', 'ex']
     if type_key == "R_16B":
         command = ['python3', 'qgemm.py', 'R_16B', 'R_16B', 'R_32F', 'R_32F', 'OP_N', 'OP_T',
-            '8640', '8640', '8640', '8640', '8640', '8640', '72', '300', 'ex']
+            '8640', '8640', '8640', '8640', '8640', '8640', '72', '30', 'ex']
 
     command[2] = type_key
     command[3] = type_key
@@ -166,7 +194,24 @@ for type_key in type_dict:
         command[13] = str(k)
 
         print(command)
+
+        stop_event = threading.Event()
+        # Create a thread to run the get_gpu_clock function in the background
+        clock_thread = threading.Thread(target=get_gpu_clock, args=(max_clock, stop_event))
+        clock_thread.daemon = True  # Set the thread as a daemon so it will exit when the main program exits
+        clock_thread.start()
+
+        print("cocotion test start exe")
         execute_program(command)
+        print("cocotion test after exe")
+
+        stop_event.set()
+        clock_thread.join()
+        # clock_thread.terminate()
+        print(f"Maximum GPU Clock: {max_clock[0]} MHz")
+        max_clock = [0]
+
+
         tflops = get_TFLOPS()
         produce_report(command, tflops, type_dict[type_key])
 
